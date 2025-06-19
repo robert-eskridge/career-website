bc2504f9b0e1915168f59ada41bb0b2441f77afc
robert-eskridge <robceskridgecareer@gmail.com>
2025-06-19 19:43:19 +0200

    website for my portfolio and career. added component pages for about, home, publications, and projects. added first bit of text on the home page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Publications } from './publications/publications';
+import { Projects } from './projects/projects'; 
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'publications', component: Publications},
+    {path: 'about', component: About},
+    {path: 'projects', component: Projects}
+];
